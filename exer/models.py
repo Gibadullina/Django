37eb0afa2d352d9ff37b1f329888a5cb301b1e37
Gibadullina <gibadullina.elina@inbox.ru>
2024-05-01 22:36:03 +0500

    #2 исправлено задание

--- exer/models.py
+++ exer/models.py
@@ -8,6 +8,8 @@ class Student(models.Model):
         verbose_name = 'Студент'
         verbose_name_plural = 'Студенты'
 
+    def __str__(self) -> str:
+        return self.name
 
 
 
@@ -17,3 +19,5 @@ class Teacher(models.Model):
     class Meta:
         verbose_name = 'Преподаватель'
         verbose_name_plural = 'Преподаватели'
+    def __str__(self) -> str:
+        return self.name
